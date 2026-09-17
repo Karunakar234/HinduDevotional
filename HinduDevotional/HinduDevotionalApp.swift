e02c8585acc4024aea8d7229079f61726e048aa6
karunakark234 <karunakark234@gmail.com>
2026-09-16 22:42:47 -0400

    Initial Commit

---
+++ HinduDevotional/HinduDevotionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HinduDevotionalApp.swift
+//  HinduDevotional
+//
+//  Created by Karunakar Katraju on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct HinduDevotionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
